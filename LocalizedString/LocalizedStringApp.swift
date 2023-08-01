47f3e1e7844a0978408f1d4c14d9d879729eff32
kenmaro3 <miha.ken.19@gmail.com>
2023-08-01 11:02:42 +0900

    Initial Commit

---
+++ LocalizedString/LocalizedStringApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedStringApp.swift
+//  LocalizedString
+//
+//  Created by Kentaro Mihara on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedStringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
